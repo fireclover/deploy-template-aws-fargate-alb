13736c8b7b1a85b2c515a499815e6c02c5246b86
Kim Daniel Engebretsen <kim@fireclover.cloud>
2025-04-02 10:21:34 +0200

    scaling for single threaded services like DBs

--- src/fargate-site.ts
+++ src/fargate-site.ts
@@ -232,6 +232,7 @@ export class FargateSite extends Construct {
         cluster, 
         taskDefinition: fargateTaskDefinition,
         minHealthyPercent: 100,
+        maxHealthyPercent: scale == 1 ? 100 : (scale+1) * 100,
         securityGroups: [tasksSecurityGroup],
         desiredCount: scale,
         assignPublicIp: true,
